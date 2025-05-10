f72e91df3ea051c0f4475c677ff33963315d4b93
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2025-05-10 10:09:40 +0700

    Tạo dự án

---
+++ TracNghiemTinHoc_Web2/src/main/java/huynbq/ntu/web2/TracNghiemTinHocWeb2Application.java
@@ -0,0 +1,13 @@
+package huynbq.ntu.web2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracNghiemTinHocWeb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracNghiemTinHocWeb2Application.class, args);
+	}
+
+}
---
+++ TracNghiemTinHoc_Web2/src/test/java/huynbq/ntu/web2/TracNghiemTinHocWeb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package huynbq.ntu.web2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracNghiemTinHocWeb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
