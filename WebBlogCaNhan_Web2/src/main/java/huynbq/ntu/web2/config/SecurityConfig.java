971f2280a7041d309394d1ce7ea42b21a47fab26
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2025-05-23 11:20:24 +0700

    Update SecurityConfig.java

--- WebBlogCaNhan_Web2/src/main/java/huynbq/ntu/web2/config/SecurityConfig.java
+++ WebBlogCaNhan_Web2/src/main/java/huynbq/ntu/web2/config/SecurityConfig.java
@@ -13,4 +13,14 @@ public class SecurityConfig {
 	 public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+	
+	 @Bean
+	    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	        http
+	            .csrf().disable()
+	            .authorizeHttpRequests(authz -> authz
+	                .requestMatchers("/**").permitAll()
+	            );
+	        return http.build();
+	    }
 }
