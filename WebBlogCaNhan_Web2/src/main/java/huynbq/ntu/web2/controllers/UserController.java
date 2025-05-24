4697d97ca65e190150df233bbccb5d0e5157f53f
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2025-05-24 22:41:07 +0700

    Update UserController.java

--- WebBlogCaNhan_Web2/src/main/java/huynbq/ntu/web2/controllers/UserController.java
+++ WebBlogCaNhan_Web2/src/main/java/huynbq/ntu/web2/controllers/UserController.java
@@ -30,6 +30,28 @@ public class UserController {
 	        model.addAttribute("error", "Mật khẩu không khớp");
 	        return "views/register";
 	    }
+	    
+	    if (!isValidUsername(username)) {
+            model.addAttribute("error", "Tên đăng nhập không hợp lệ. " +
+                    "Tên đăng nhập phải dài 3-30 ký tự, " +
+                    "chỉ chứa chữ cái, số, dấu chấm hoặc dấu gạch dưới, " +
+                    "và không được bắt đầu hoặc kết thúc bằng dấu chấm hoặc dấu gạch dưới.");
+            return "views/register";
+        }
+
+        if (!password.equals(confirmPassword)) {
+            model.addAttribute("error", "Mật khẩu không khớp");
+            return "views/register";
+        }
+        
+        
+
+        // Kiểm tra điều kiện mật khẩu
+        if (!isValidPassword(password)) {
+            model.addAttribute("error", "Mật khẩu phải dài ít nhất 9 ký tự, có chữ hoa, chữ thường và ký tự đặc biệt");
+            return "views/register";
+        }
+
 
 	    // Kiểm tra điều kiện mật khẩu
 	    if (!isValidPassword(password)) {
@@ -50,6 +72,11 @@ public class UserController {
 
 	    return "redirect:/blog/login";
 	}
+	
+	  private boolean isValidUsername(String username) {
+	        String pattern = "^(?![_.])[a-zA-Z0-9._]{3,30}(?<![_.])$";
+	        return username.matches(pattern);
+	    }
 
 	// Hàm kiểm tra mật khẩu hợp lệ
 	private boolean isValidPassword(String password) {
